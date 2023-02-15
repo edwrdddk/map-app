d213bd895cdd9a97cd06d3d43b56092774c13dc7
Edward Didyk <edwarddidyk@MacBook-Air-Edward.local>
2023-02-15 15:54:31 +0200

    test 1

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+const port = 8800;
+const userRoute = require('./routes/users');
+const pinRoute = require('./routes/pins');
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
+.then(() => {
+  console.log("MongoDB Connected");
+})
+.catch((err) => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+});

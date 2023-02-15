f7f4536779e506efb4c23ad957fc6b15dfdcb55f
Edward Didyk <edwarddidyk@MacBook-Air-Edward.local>
2023-02-15 20:40:58 +0200

    frontend: idk y I deleted gitignore, but now its back + mapbox api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
+import * as React from 'react';
+import Map, { Marker } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import PlaceIcon from '@mui/icons-material/Place';
+
 function App() {
+  const [viewState, setViewState] = React.useState({
+    longitude: 30,
+    latitude: 50,
+    zoom: 3.5
+  });
   return (
     <div className="App">
-      hello
+      <Map
+        {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      >;
+        <Marker longitude={40} latitude={2}/>
+
+      </Map>;
     </div>
   );
 }

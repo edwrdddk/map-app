ccbc2ae1cc951657b92b6cb1c96d1bf29770648f
Edward Didyk <edwarddidyk@MacBook-Air-Edward.local>
2023-02-16 17:43:43 +0200

    Add marker and pop up. Failing to get data from backend axios.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,33 @@
 import * as React from 'react';
-import Map, { Marker } from 'react-map-gl';
+// import { useState, useEffect} from 'react';
+import Map, { Marker, Popup } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import PlaceIcon from '@mui/icons-material/Place';
+import StarIcon from '@mui/icons-material/Star';
+import "./app.css"
+const axios = require('axios').default;
+
 
 function App() {
+  const [pins, setPins] = React.useState([]);
   const [viewState, setViewState] = React.useState({
     longitude: 30,
     latitude: 50,
     zoom: 3.5
   });
+
+  React.useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins()
+  }, []);
+
   return (
     <div className="App">
       <Map
@@ -17,10 +36,34 @@ function App() {
         style={{ width: "100vw", height: "100vh" }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-      >;
-        <Marker longitude={40} latitude={2}/>
-
-      </Map>;
+      >
+        {pins.map(p => (
+          <>
+            <Marker latitude={p.lat} longitude={p.long}>
+              <PlaceIcon style={{ fontSize: viewState.zoom * 8, color: "slateblue" }} />
+            </Marker>
+            <Popup latitude={p.lat} longitude={p.long} anchor="left" closeOnClick={false}>
+              <div className="card">
+                <label>Place</label>
+                <h3>Le Café des Chats</h3>
+                <label>Review</label>
+                <p className="desc">Nice place.</p>
+                <label>Rating</label>
+                <div className="Stars">
+                  <StarIcon className="star" />
+                  <StarIcon className="star" />
+                  <StarIcon className="star" />
+                  <StarIcon className="star" />
+                  <StarIcon className="star" />
+                </div>
+                <label>Information</label>
+                <span className="username">Created by <b>Finn</b></span>
+                <span className="date">1 hour ago</span>
+              </div>
+            </Popup>
+          </>
+        ))};
+      </Map>
     </div>
   );
 }
